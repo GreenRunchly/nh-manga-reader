10d634e1a154448f6225876efe303dca0c859909
Rizki Irfan Anshori <ilfan.indo@gmail.com>
2022-02-07 12:46:08 +0700

    Pembaharuan

---
+++ config.php
@@ -0,0 +1,49 @@
+<?php
+
+    define('URL_SITUS_MASTER', 'http://nh.local');
+    define('URL_SITUS', 'http://nh.local');
+    define('DIR_SITUS', $_SERVER['DOCUMENT_ROOT']);
+
+    // Function to get the client IP address
+	function ngambilIp() {
+	    $ipaddress = '';
+	    if (getenv('HTTP_CLIENT_IP'))
+	        $ipaddress = getenv('HTTP_CLIENT_IP');
+	    else if(getenv('HTTP_X_FORWARDED_FOR'))
+	        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+	    else if(getenv('HTTP_X_FORWARDED'))
+	        $ipaddress = getenv('HTTP_X_FORWARDED');
+	    else if(getenv('HTTP_FORWARDED_FOR'))
+	        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+	    else if(getenv('HTTP_FORWARDED'))
+	       $ipaddress = getenv('HTTP_FORWARDED');
+	    else if(getenv('REMOTE_ADDR'))
+	        $ipaddress = getenv('REMOTE_ADDR');
+	    else
+	        $ipaddress = 'UNKNOWN';
+	    return $ipaddress;
+	}
+
+	function get_string_between($string, $start, $end){
+	    $string = ' ' . $string;
+	    $ini = strpos($string, $start);
+	    if ($ini == 0) return '';
+	    $ini += strlen($start);
+	    $len = strpos($string, $end, $ini) - $ini;
+	    return substr($string, $ini, $len);
+	}
+
+	function get_http_response_code($url) {
+		$headers = get_headers($url);
+		return substr($headers[0], 9, 3);
+	}
+	
+	function getUrl($url){
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }   
+    
+?>
---
+++ style/script.js
@@ -0,0 +1,50 @@
+/* System Functions */
+function setData(input,pada){
+    /* simpan data */
+    localStorage.setItem(pada, input);
+    console.log('Set Data "'+pada+'"');
+}  
+function loadData(pada){
+    /* load data */
+    return localStorage.getItem(pada);
+    console.log('Load Data "'+pada+'"');
+}
+function deleteData(pada){
+    /* load data */
+    return localStorage.removeItem(pada);
+    console.log('Delete Data "'+pada+'"');
+}
+function appSettingsLoad(){
+    var maintenance = loadData('maintenance');
+    if (maintenance != 'clicked'){
+        $("#modalmaintenance").removeClass("off");
+        $("#modalmaintenance").addClass("on");
+    }
+    //console.log(maintenance);
+    console.log('Settings Loaded');
+}
+function salinInput() {
+    var copyText = document.getElementById("codeinput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
+function getPesan(content){
+    $("#modalpesan .modal-content p").html(content);
+    $("#modalpesan").removeClass("off");
+    $("#modalpesan").addClass("on");
+};
+$(document).on('click','.toggle',function(e){
+    var idnya = $(this).attr("toggle");
+    var has = $("#"+idnya).hasClass('on');         
+    if (has == true){
+        $("#"+idnya).addClass("off");
+        $("#"+idnya).removeClass("on");
+        //console.log('off');
+        setData('clicked','maintenance');
+    }else{
+        $("#"+idnya).addClass("on");
+        $("#"+idnya).removeClass("off");
+        //console.log('on');  
+    }
+});
